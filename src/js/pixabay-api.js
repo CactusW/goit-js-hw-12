d648a012235e5043f82670699546f33eef158e0e
Nick <chernyavskij5@gmai.com>
2024-08-28 18:09:56 +0300

    base_2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,34 @@
-const URL = "https://pixabay.com/api/";
-const API_KEY = "45561699-87bf7fcf520d3d41b811bd2a3";
+/* Підключення бібліотеки axios - є це простий HTTP-клієнт, що базується на Promise і автоматизує багато рутинних завдань при роботі з HTTP-запитами, а саме дозволяє:
+- зберігати глобальні налаштування, що будуть автоматично додаватися до всіх запитів;
+- автоматично перетворювати дані запиту у формат JSON;
+- парсити дані відповіді з формату JSON;
+- обробляти всі можливі помилки запиту, включаючи 404, та багато іншого.
+- Axios є зручною альтернативою стандартному Fetch API.
+ */
+import axios from 'axios';   
 
+const URL = "https://pixabay.com/api/";                  // шлях до галереї pixabay
+const API_KEY = "45296804-0fb55f0e1381bd4cbf585a7a5";    // ключ що отрімала після реєстрації тут https://pixabay.com/api/docs/#api_search_images
 
+axios.defaults.baseURL = URL;
 
-export function fetchImages(query) {
-    const params = new URLSearchParams({
+// Робимо запит на сервер для пошуку колекції картинок
+export async function fetchImages(query, page = 1) {      
+    const params = {
         key: API_KEY,
         q: query,
         image_type: "photo",
         orientation: "horizontal",
-        safesearch: true,  
-    });
+        safesearch: true,
+        per_page: 15,                                     // pageSize замінює per_Page для цієї галереї
+        page: page,                                            
+    };
 
-   
-    return fetch(`${URL}?${params.toString()}`)    
-    .then(response => {                          
-        if (!response.ok) {                        
-            throw new Error(response.statusText);      
-        }
-        return response.json();                 
-    })
-
-.catch(error => {
+try {
+    const response = await axios.get(URL, {params});      
+    return response.data;
+} catch(error) {
     console.error("Image Search Error:", error);
     throw error;
-});
 }
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import {fetchImages} from './js/pixabay-api.js';
 import {
   renderImages,
@@ -6,30 +5,29 @@ import {
   showLoader,
   hideLoader, 
 } from './js/render-functions.js';
-// Описаний у документації
+// Підключення бібліотеки для повідомлень - помилки, закінчення колекції
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
+// стилі для повідомлень
 import "izitoast/dist/css/iziToast.min.css";
 
 
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+
+
 // 2) Користувач буде вводити рядок для пошуку в текстове поле, а за сабмітом форми необхідно виконувати HTTP-запит із цим пошуковим рядком.
-
 const form = document.querySelector(".form-search");
+let query = ''; // Змінна для зберігання запиту пошуку
+let page = 1; // Поточна сторінка
+let maxHits = 0; // Змінна для зберігання загальної кількості знайдених зображень
+let totalHits = 0; // Змінна для відстеження кількості вже відображених зображень
 
-form.addEventListener("submit", event => {
+// Обробник події при сабміті форми
+form.addEventListener("submit", async event => {
     event.preventDefault();
     //Обробка тексту що було введено користувачем в форму :
-
-    // 1. Отримаємо посилання на input з іменем query (const queryInput):
-    //   1.1 event.currentTarget - посилання на поточний елемент що обробляє подію - на форму;
-    //    1.2.elements - колекція елементів в середині форми(input);
-    //    1.3 query - ім'я елемента форми (input з name="query").
-    const queryInput = event.currentTarget.elements.query;
-    // 2. Отримуємо запит користувача і очищуємо його від зайвих пробілів (const query):
-    //    2.1 queryInput.value - отримання значення (текст), введене користувачем в input
-    //    2.2. trim() - видаляємо пробіли з початку і кінця веденного значення
-    const query = queryInput.value.trim().toLowerCase();
-
+query = event.currentTarget.elements.query.value.trim(); // Отримання значення з інпуту
+page = 1; // Повернення на першу сторінку
+totalHits = 0;  // Скидання лічильника відображених зображень                                              // Reset the total hits counter
 
     // 3) При натисканні на кнопку відправки форми, додайте перевірку вмісту текстового поля на наявність порожнього рядка, щоб користувач не міг відправити запит, якщо поле пошуку порожнє.
     if(!query) {                           //якщо немає тексту в input то:
@@ -41,26 +39,163 @@ form.addEventListener("submit", event => {
     }
 
 
-clearGallery();
-showLoader();
+clearGallery();        // Очищення галереї                            
+showLoader();         // Показати індикатор завантаження
+loadMoreBtn.classList.add('hidden');  // Приховати кнопку "Load more"
 
-fetchImages(query)
-.then(data => {
-    if(data.hits.length === 0) {
+try {
+  const data = await fetchImages(query, page);
+  maxHits = data.totalHits;  // Збереження загальної кількісті знайдених зображень
+
+
+  if(data.hits.length === 0) { //Якщо масив hits порожній, відображається повідомлення про те, що зображень не знайдено.
         iziToast.error({
 message: 
 "Sorry, there are no images matching your search query. Please try again!",
 position: 'topRight',
         });
     } else {
-        renderImages(data.hits);
-    }
-})
-.catch(error => {
-  iziToast.error({ title: 'Error', message: error.message });
-})
-.finally(() => {
-  hideLoader();
-  queryInput.value = '';
+      totalHits += data.hits.length; // Оновлення кількісті відображених зображень
+        renderImages(data.hits); // Відображення зображення
+
+    if (totalHits >= maxHits) {  // Якщо всі зображення відображено
+      iziToast.info({
+message: "We're sorry, but you've reached the end of search results.",
+position: 'topRight',
 });
-});//Якщо масив hits порожній, відображається повідомлення про те, що зображень не знайдено. Якщо ж зображення знайдено, вони рендеряться за допомогою функції renderImages.
+} else {
+  loadMoreBtn.classList.remove('hidden');  // Показуємо кнопку "Load more", якщо є ще зображення
+}
+      }
+} catch(error) {
+  iziToast.error({ title: 'Error', message: error.message }); //якщо виникають помилки запиту - відображаємо за допомогою бібліотеки
+} finally {
+  hideLoader();                                               // Приховуємо індикатор завантаження
+  form.reset();                                               // Очистити форму після запиту
+}
+});
+
+
+
+//---------------------------------------------------------------------------------------------------------//
+//^     Кнопка "завантажити ще"
+
+//1. Якщо данних немає - то і кнопки не має.
+//2. Якщо користувач вже натиснув на кнопку, то спочатку повинні загрузитись данні, а тоді можна клікнути ще раз. Користувач не може клікати 1000разів.
+//3. Якщо дійшли до кінця галереї то кнопку треба видалити.
+
+
+/*
+1. блокуємо кнопку
+2. додаємо до сторінки +1
+3. робимо запит
+4. отримаємо відповідь та візуалізуємо карточки
+5. після запиту робимо перевірку: якщо поточна сторінка співпадає з максимальною - то ховаємо кнопку і знімаємо слухача подій, якщо ж все ок - розблоковуємо кнопку для подальшої взаємодії
+
+*/
+// Обробник події при натисканні кнопки "Load more"
+loadMoreBtn.addEventListener("click", async () =>{         //якщо натиснули на Кнопку "завантажити ще" то відображається ще одна нова сторінка
+  page+=1;                                                 // Перехід на наступну сторінку
+  showLoader();                                            // Показування індикатора завантаження
+
+  try{
+    const data = await fetchImages(query, page);
+    totalHits += data.hits.length;                          // Оновлення кількісті відображених зображень      
+    renderImages(data.hits);                                // Відображення нових зображень
+    smoothScroll();                                         // Плавне прокручування до нових зображень
+
+    if (totalHits >= maxHits) {                              // Якщо всі зображення відображено
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+      loadMoreBtn.classList.add('hidden');                   // Приховати кнопку "Load more"
+    }
+
+  } catch(error) {
+    iziToast.error({ title: 'Error', message: error.message });
+  } finally {
+    hideLoader();                                                 // Приховати індикатор завантаження
+  }
+});
+
+
+// Функція для плавного прокручування до нових зображень
+function smoothScroll() {
+  const { height: cardHeight } = document
+  .querySelector('.gallery-list')
+  .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Пагінація: номер групи та кількість елементів в групі
+// - Завантажуємо галерею при сабміті форми
+// - Завантажуємо галерею при натискані на кнопку заватажити ще
+// - Оновлюємо групу в параментрах запиту
+// - Рендерим галерею
+// - Скидання значення при пошуку за новим критерієм
+// - Показуємо спінер поки йде запит
+
+
+//створюємо обект де зберігаємо данні(page, perPage, q, maxPage)
+
+// 1. сховати кнопку "завантажити ще"
+// 2. вішає
+
+
+//load more btn stages 
+// 1. hidden - ховаємо кнопку коли скінчилися результати сторінок, або коли їх взагалі немає, або ми перший раз на сторінці
+// 2. visible - коли робимо новий запит
+// 3. disable - відключаємо тоді коли робимо запит на сервер і чекаємо відповіді від нього
+// 4. enabled - включаемо кнопку тоді коли отримали результати запиту
+
+/* const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+const spinner = document.querySelector(".spinner");
+const hiddenClass = "is-hidden"; //функція для приховання кнопки
+
+class ButtonService{
+constructor(buttonEL, hiddenClass) {
+  this.buttonEL = buttonEL;
+  this.hiddenClass = hiddenClass;
+}
+hide(){
+  this.buttonEL.classList.add(this.hiddenClass);
+}
+show(){
+  this.buttonEL.classList.remove(this.hiddenClass);
+}
+disable(){
+  this.buttonEL.disabled = true;
+}
+enable(){
+  this.buttonEL.disabled = false}
+}
+
+const loadMoreBtnEl = new ButtonService(loadMoreBtn, "is-hidden");
+console.log(loadMoreBtnEl);
+loadMoreBtnEl.disable();
+ */
